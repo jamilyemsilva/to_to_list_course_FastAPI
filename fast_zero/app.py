b51c041b56d2476c9841b4e67ae4d799752c5dde
Jamily <dev.jamilyemanuele@gmail.com>
2026-07-08 13:54:25 -0300

    Criando teste e schemas preliminares

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,30 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get('/exercicio_html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def exercicio_html():
+    return """
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Olá mundo em python usando html</title>
+            </head>
+            <body>
+              <p>'Olá Mundo'</p> 
+            </body>
+            </html>            
+          """
